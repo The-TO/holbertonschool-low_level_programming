ddcb168e5fcdf9d00fcdde991d8c7ed59636fc6a
The-TO <theovinc@gmail.com>
2026-06-22 16:39:53 +0200

    add: create 5-printf program to print a message

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main - print with printf some text
+ *
+ *Return: 0 (Succes)
+ */
+
+int main(void)
+{
+printf("\"Programming is like building a multilingual puzzle");
+return (0);
+}
