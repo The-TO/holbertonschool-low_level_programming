37d42299599d6bf4862de6905caf390976932331
The-TO <theovinc@gmail.com>
2026-06-22 16:31:40 +0200

    fix: format code in 4-puts program for consistency

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+
+/**
+ *main - print with puts some text
+ *
+ *Return: 0 (Succes)
+ */
+
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
