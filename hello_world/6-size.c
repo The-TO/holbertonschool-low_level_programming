d9cb5af230baf0f2b87a8e8b361ea690a2b2b9ea
The-TO <theovinc@gmail.com>
2026-06-22 16:55:05 +0200

    add: create 6-size program to print size of a char

---
+++ hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main - print with printf size of character
+ *
+ *Return: 0 (Succes)
+ */
+
+int main(void)
+{
+printf("Size of a char: %d byte(s)\n", sizeof(int));
+return (0);
+}
