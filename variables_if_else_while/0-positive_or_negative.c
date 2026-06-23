7587175f708d7db87852d0aedf9bf3bc4eb375c9
The-TO <theovinc@gmail.com>
2026-06-23 13:14:15 +0200

    feat: implement positive or negative number checker

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d est positif\n ", n);
+
+	}
+	else if (n == 0)
+
+	{
+		printf("0\n");
+
+	}
+	else
+	{
+
+		printf("%d est negatif\n" , n);
+
+	}
+
+	return (0);
+}
