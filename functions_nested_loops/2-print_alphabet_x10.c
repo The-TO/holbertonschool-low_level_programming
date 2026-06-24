58ca108fff966970e64c65ab9fa2d94dc713c495
The-TO <theovinc@gmail.com>
2026-06-24 14:56:54 +0200

    fix: correct comment formatting for print_alphabet_x10 function

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include"main.h"
 /**
- * -print_alphabet_x10 - code to print alphabet 10 times
+ * print_alphabet_x10 - code to print alphabet 10 times
  *
  * Return: 0 (Succes)
  */
