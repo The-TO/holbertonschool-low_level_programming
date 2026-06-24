9ddf8d0ae7a7d4d0c8c13e595f9cd02ca9164b8a
The-TO <theovinc@gmail.com>
2026-06-24 14:55:33 +0200

    feat: add print_alphabet_x10 function and main to demonstrate its usage

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include"main.h"
+/**
+ * -print_alphabet_x10 - code to print alphabet 10 times
+ *
+ * Return: 0 (Succes)
+ */
+void print_alphabet_x10(void)
+{
+	char letter = 'a';
+	int i = 0;
+while (i <= 9)
+{			letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar (letter);
+		if (letter == 'z')
+		{
+			_putchar ('\n');
+		}
+		letter++;
+
+	}
+i++;
+}
+
+}
