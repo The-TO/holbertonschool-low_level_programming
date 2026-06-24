6b0bd3f782f3d875cf29f6d2da9340ae2c9535f8
The-TO <theovinc@gmail.com>
2026-06-24 14:15:02 +0200

    feat: add print_alphabet function to print the alphabet using _putchar

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *  main - code for print alphabet with my proper putchar
+ *
+ * Return: 0 (Succes)
+ */
+
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar (letter);
+		letter++;
+
+	}
+	_putchar ('\n');
+}
