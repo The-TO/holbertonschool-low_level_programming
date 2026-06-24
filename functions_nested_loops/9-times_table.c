fdf7f2648efdada3945d1d194f0a2a0a95697a99
The-TO <theovinc@gmail.com>
2026-06-24 23:45:30 +0200

    star 9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *time table - work to print time table
+ *
+ *Return: 0 (Succes)
+ */
+void times_table(void)
+{
+    i = 1
+    j = 1
+    int row
+    int col
+    
+    while ()
+}
