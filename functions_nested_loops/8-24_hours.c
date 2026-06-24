8159cd64b1dd07b16150fa7ab70d7546bd7c5d02
The-TO <theovinc@gmail.com>
2026-06-24 23:28:44 +0200

    add jack bauer command

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ *jack_bauer - work to print each minutes of the day
+ *
+ *Return: 0 (Succes)
+ */
+void jack_bauer(void)
+{
+	int hour = 0;
+
+	int i = 0;
+
+	while (hour <= 24)
+	{
+		int minute = 0;
+
+		while (minute <= 59)
+		{
+			_putchar ((hour / 10) + '0');
+			_putchar ((hour % 10) + '0');
+			_putchar (':');
+			_putchar ((minute / 10) + '0');
+			_putchar ((minute % 10) + '0');
+			minute++;
+			_putchar('\n');
+		}
+		hour++;
+	}
+	i++;
+_putchar('\n');
+}
