cbea803ca48fed2f5e3bdd85f570dbb6257531e0
The-TO <theovinc@gmail.com>
2026-06-25 20:27:20 +0200

    add mul function to multiply two integers and update header file

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - is a fonction make to multiple two int
+ *
+ * @a: is the frist value to multiplies
+ * @b: is the other value to multiplies
+ *
+ * Return: 0 (Succes)
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
