45e9bb9306151713be63a3e8749408f33ba94918
The-TO <theovinc@gmail.com>
2026-06-25 22:27:30 +0200

    add print_diagonal function and update header file

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_diagonal - is a fonction print number on 14 lines
+ *
+ * @n: is the number of line we want to print
+ *
+ * Return: 0 (Succes)
+ */
+void print_diagonal(int n)
+{
+	int line;
+	int space;
+
+	line = 0;
+	while (line < n)
+	{
+		space = 0;
+		while (space < line)
+		{
+			_putchar (' ');
+		space++;
+		}
+		_putchar ('\\');
+		_putchar ('\n');
+		line++;
+	}
+}
