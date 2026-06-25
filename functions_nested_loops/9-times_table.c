d4c72a86af345dde65d5a413f841b2848f1b3de3
The-TO <theovinc@gmail.com>
2026-06-25 10:35:59 +0200

    add times_table function to print multiplication table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,15 +1,37 @@
 #include "main.h"
 /**
- *time table - work to print time table
+ *times_table - work to print time table
  *
  *Return: 0 (Succes)
  */
 void times_table(void)
 {
-    i = 1
-    j = 1
-    int row
-    int col
-    
-    while ()
+	int i;
+	int j;
+
+	for (j = 0; j <= 9; j++)
+	{
+		for (i = 0; i <= 9; i++)
+		{
+			if (i == 0)
+			{
+				_putchar (i * j + '0');
+			}
+			else if (i * j >= 10)
+			{
+				_putchar (',');
+				_putchar (' ');
+				_putchar (i * j / 10 + '0');
+				_putchar (i * j % 10 + '0');
+			}
+			else
+			{
+				_putchar (',');
+				_putchar (' ');
+				_putchar (' ');
+				_putchar(i * j + '0');
+			}
+			}
+	_putchar('\n');
+	}
 }
