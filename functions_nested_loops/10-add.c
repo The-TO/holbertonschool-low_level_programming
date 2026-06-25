d7f70eb765cd36cb9fa0897dea926980ed67c18b
The-TO <theovinc@gmail.com>
2026-06-25 10:48:32 +0200

    add add function and main to demonstrate addition

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *add - work to make a code who do an additon
+ *
+ * @a: the first value
+ *
+ * @b: the second value
+ *
+ *Return: 0 (Succes)
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
