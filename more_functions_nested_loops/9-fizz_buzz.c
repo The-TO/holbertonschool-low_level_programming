051c8b47b45c1f246ce9e93f229dab2e8f00135a
The-TO <theovinc@gmail.com>
2026-06-26 13:16:05 +0200

    add fizz_buzz function to print numbers from 1 to 100 with FizzBuzz logic

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include<stdio.h> 
+
+int main (void)
+{
+    int number;
+    for (number = 1 ; number <= 100 ; number ++)
+    {
+        if (number%15 == 0)
+        {
+            printf(" FizzBuzz ");
+        }
+        else if (number%5 == 0)
+        {
+            printf(" Buzz ");
+        }
+        else if (number%3 == 0)
+        {
+            printf(" Fizz ");
+        }
+        else
+        {
+            printf("%d ", number);
+
+        }
+        
+
+    }
+    return (0);
+}
