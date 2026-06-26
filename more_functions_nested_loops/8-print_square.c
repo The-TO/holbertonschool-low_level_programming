aec3533f3043aa200238547fb573cd73670af149
The-TO <theovinc@gmail.com>
2026-06-26 11:06:20 +0200

    add print_square function and update main header file

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_square - draws square with #
+ * @size: number #
+ */
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	if (size <= 0)
+
+	{
+		_putchar('\n');
+	}
+	for (i = 0 ; i < size ; i++)
+	{
+		for (j = 0; j < size ; j++)
+		{
+			_putchar ('#');
+		}
+		 _putchar('\n');
+	}
+}
+
