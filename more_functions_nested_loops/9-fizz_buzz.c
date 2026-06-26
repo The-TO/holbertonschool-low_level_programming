0cb93cc064ebfa5291885b14e161c3757bd32519
The-TO <theovinc@gmail.com>
2026-06-26 13:36:00 +0200

    fix FizzBuzz output formatting for consistent spacing

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -10,21 +10,25 @@ int main(void)
 
 	for (number = 1 ; number <= 100 ; number++)
 	{
+		if (number != 1)
+		{
+			printf(" ");
+		}
 		if (number % 15 == 0)
 		{
-			printf(" FizzBuzz");
+			printf("FizzBuzz");
 		}
 		else if (number % 5 == 0)
 		{
-			printf(" Buzz");
+			printf("Buzz");
 		}
 		else if (number % 3 == 0)
 		{
-			printf(" Fizz");
+			printf("Fizz");
 		}
 		else
 		{
-			printf("%d ",number );
+			printf("%d ",number);
 
 		}
 	}
