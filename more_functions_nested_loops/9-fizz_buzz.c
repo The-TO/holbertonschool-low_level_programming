99f92eada9318bbadf49e4c0475e348a9898bfb7
The-TO <theovinc@gmail.com>
2026-06-26 13:22:47 +0200

    fix formatting in FizzBuzz output to remove leading spaces

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -12,15 +12,15 @@ int main(void)
 	{
 		if (number % 15 == 0)
 		{
-			printf(" FizzBuzz ");
+			printf("FizzBuzz ");
 		}
 		else if (number % 5 == 0)
 		{
-			printf(" Buzz ");
+			printf("Buzz ");
 		}
 		else if (number % 3 == 0)
 		{
-			printf(" Fizz ");
+			printf("Fizz ");
 		}
 		else
 		{
