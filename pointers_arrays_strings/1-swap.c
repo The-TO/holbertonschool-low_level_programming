87bf69f37c6f00b091e4f587e11f6f572ceb0705
The-TO <theovinc@gmail.com>
2026-06-30 13:37:32 +0200

    fix: correct formatting and add newline in swap_int function

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -3,15 +3,16 @@
  * swap_int - is a fonction that swap the value of a and b
  *
  * @a: is the first value
- * 
+ *
  * @b: is the second value
  *
  * Return: 0 (Succes)
  */
 void swap_int(int *a, int *b)
 {
-    int temp;
-    temp = *a;
-    *a = *b;
-    *b = temp;
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
 }
