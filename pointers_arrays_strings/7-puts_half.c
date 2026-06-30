bd8f9fe5d12e46b7030c1d999b5865148456f251
The-TO <theovinc@gmail.com>
2026-06-30 23:10:06 +0200

    feat: implement puts_half function to print half of a string and update main.h

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * puts_half - print only pair number
+ *
+ * @str: is the value in the string
+ *
+ * Return: Always 0.
+ */
+void puts_half(char *str)
+{
+	int i;
+	int start;
+
+	for (i = 0 ; str[i] != '\0' ; i++)
+	{}
+	if (i % 2 == 0)
+	{
+		start = i / 2;
+	}
+	else
+	{
+		start = (i - 1) / 2;
+	}
+	for (; start != i; start++)
+	{
+		_putchar(str[start]);
+	}
+	_putchar ('\n');
+
+}
