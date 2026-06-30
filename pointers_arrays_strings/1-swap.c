d22ccb7580ce22339a82b03c5511980b30ae3e04
The-TO <theovinc@gmail.com>
2026-06-30 13:36:25 +0200

    feat: add swap_int function and main test file

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * swap_int - is a fonction that swap the value of a and b
+ *
+ * @a: is the first value
+ * 
+ * @b: is the second value
+ *
+ * Return: 0 (Succes)
+ */
+void swap_int(int *a, int *b)
+{
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
