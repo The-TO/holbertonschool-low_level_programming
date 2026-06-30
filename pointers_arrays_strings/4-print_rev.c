b21c76bb0a1aa12fc3d07fbbbf2fbc46aee67162
The-TO <theovinc@gmail.com>
2026-06-30 16:35:19 +0200

    feat: add print_rev function and main test file

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_rev(char *s)
+{
+    int i;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+	{}
+	return (number);
+}
