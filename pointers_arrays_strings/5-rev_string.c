7434d91103d30940d42d6d2dc6311e5baf0560bb
The-TO <theovinc@gmail.com>
2026-06-30 22:09:10 +0200

    style: update documentation for rev_string function

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,26 +1,27 @@
 #include "main.h"
 
 /**
- * rev_string.c - print the var reversed
+ * rev_string - print the string reversed
  *
- * @s: is the value of the var
+ * @s: is the value of the string
  *
  * Return: Always 0.
  */
 void rev_string(char *s)
 {
-    char temp;
-    int i;
-    int start;
-    int end;
-    for (i = 0 ; s[i] != '\0' ; i++)
-    {}
-    i--;
-    end = i;
-    for (start = 0 ; start < end ; start++ , end--)
-    {
-        temp = s[start];
-        s[start] = s[end];
-        s[end] = temp;
-    }
+	char temp;
+	int i;
+	int start;
+	int end;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+	{}
+	i--;
+	end = i;
+	for (start = 0 ; start < end ; start++, end--)
+	{
+		temp = s[start];
+		s[start] = s[end];
+		s[end] = temp;
+	}
 }
