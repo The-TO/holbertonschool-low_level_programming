96bab48cc1bd1e07148a243128c0e6e8baf5f73c
The-TO <theovinc@gmail.com>
2026-07-01 15:27:51 +0200

    fix: correct logic in _strncpy to properly handle null termination

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -11,16 +11,19 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
+	int fini;
 
 	for (i = 0 ; i < n ; i++)
 	{
-		if (src[i] != '\0')
+		fini = 0
+		if (fini == 0 && src[i] != '\0')
 		{
 			dest[i] = src[i];
 		}
 		else
 		{
 			dest[i] = '\0';
+			fini = 1;
 		}
 	}
 	return (dest);
