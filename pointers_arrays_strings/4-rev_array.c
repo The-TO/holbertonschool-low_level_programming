bc20a1bfde5edfe6aff95846212481336afc028e
The-TO <theovinc@gmail.com>
2026-07-01 16:49:17 +0200

    feat: implement print_array and reverse_array functions, update main.h

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reverse_array - Reverse reverse the content of an array
+ * @a: Pointeur vers le tableau d'entiers
+ * @n: Le nombre d'éléments dans le tableau
+ *
+ *
+ * Return: void (Aucun retour)
+ */
