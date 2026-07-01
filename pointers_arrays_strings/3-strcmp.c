804267d6694490fd83ac4f17c53155ca04ec8fc0
The-TO <theovinc@gmail.com>
2026-07-01 16:01:57 +0200

    feat: add _strcmp function to compare two strings and update main.h

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compare s1 and  s2
+ * 
+ * @s1: La première chaîne à comparer.
+ * @si2: La deuxième chaîne à comparer.
+ *
+ * Return: Un entier indiquant la relation entre les chaînes
+ */
+int _strcmp(char *s1, char *s2)
+[
+    int i;
+
+    for (i = 0; s1[i] == s2[i] ; i++)
+]
