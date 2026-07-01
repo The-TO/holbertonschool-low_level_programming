4b03fdab8182b7171a30a2ba577cfa2820b2dac5
The-TO <theovinc@gmail.com>
2026-07-01 16:13:34 +0200

    fix: correct syntax errors in _strcmp function and ensure proper return statement

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -9,8 +9,14 @@
  * Return: Un entier indiquant la relation entre les chaînes
  */
 int _strcmp(char *s1, char *s2)
-[
+{
     int i;
 
     for (i = 0; s1[i] == s2[i] ; i++)
-]
+    {
+        if (s1[1] == '\0' && s2[i] == '\0')
+        {
+            return(0)
+        }
+    }
+}
