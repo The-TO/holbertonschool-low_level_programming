a8079791c962e74f70c53baadaa4bced049d1b00
The-TO <theovinc@gmail.com>
2026-07-01 13:57:18 +0200

    fix: correct function documentation and formatting in _strcat

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,22 +1,22 @@
 #include "main.h"
 /**
- * reset_to_98 - is a fonction to rester a var to 98 with pointers
- *
- * @n: is the pointer
+ * _strcat - Concatenates two strings
+ * @dest: The destination string
+ * @src: The source string to append
  *
  * Return: 0 (Succes)
  */
 char *_strcat(char *dest, char *src)
 {
-    int i;
-    int s;
-    
-    for (i = 0 ; dest[i] != '\0' ; i++)
-    ;
-    for (s = 0 ; src[s] != '\0' ; s++)
-    {
-        dest[i] = src[s];
-        i++;
-    } 
-    return (dest);
+	int i;
+	int s;
+
+	for (i = 0 ; dest[i] != '\0' ; i++)
+	;
+	for (s = 0 ; src[s] != '\0' ; s++)
+	{
+		dest[i] = src[s];
+		i++;
+	}
+	return (dest);
 }
