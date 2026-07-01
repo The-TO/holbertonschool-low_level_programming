3aaf5b818971965272421c91f72aea1e54390ca0
The-TO <theovinc@gmail.com>
2026-07-01 16:34:19 +0200

    fix: correct parameter name and logic in _strcmp function

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -2,21 +2,22 @@
 
 /**
  * _strcmp - Compare s1 and  s2
- * 
+ *
  * @s1: La première chaîne à comparer.
- * @si2: La deuxième chaîne à comparer.
+ * @s2: La deuxième chaîne à comparer.
  *
  * Return: Un entier indiquant la relation entre les chaînes
  */
 int _strcmp(char *s1, char *s2)
 {
-    int i;
+	int i;
 
-    for (i = 0; s1[i] == s2[i] ; i++)
-    {
-        if (s1[1] == '\0' && s2[i] == '\0')
-        {
-            return(0)
-        }
-    }
+	for (i = 0; s1[i] == s2[i] ; i++)
+	{
+		if (s1[i] == '\0')
+		{
+			return (0);
+		}
+	}
+	return (s1[i] - s2[i]);
 }
