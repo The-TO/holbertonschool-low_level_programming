0f56127a8a33e889821b0b0329638dae123cd963
The-TO <theovinc@gmail.com>
2026-07-02 17:02:08 +0200

    feat: implement print_diagsums function to calculate and print the sums of the two diagonals of a square matrix

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -10,7 +10,6 @@
  */
 
 void print_diagsums(int *a, int size)
-// Fonction qui affiche la somme des deux diagonales d'une matrice carrée
 {
 	int i;
 	int j;
@@ -19,15 +18,15 @@ void print_diagsums(int *a, int size)
 
 	sum1 = 0;
 	sum2 = 0;
-	for (i = 0; i < size; i++) // Parcourt les lignes de la matrice
+	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < size; j++) // Parcourt les colonnes
+		for (j = 0; j < size; j++)
 		{
 			if (i == j)
-				sum1 += a[i * size + j]; // Somme de la diagonale principale
+				sum1 += a[i * size + j];
 			if (i + j == size - 1)
-				sum2 += a[i * size + j]; // Somme de la diagonale secondaire
+				sum2 += a[i * size + j];
 		}
 	}
-	printf("%d, %d\n", sum1, sum2); // Affiche les deux sommes
+	printf("%d, %d\n", sum1, sum2);
 }
