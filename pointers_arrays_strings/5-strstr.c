51bcc5e0edcf566b6cf7825456ffc11fef50fcaa
The-TO <theovinc@gmail.com>
2026-07-02 16:58:12 +0200

    feat: implement print_chessboard function and update main to demonstrate usage

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -10,24 +10,28 @@
  * or NULL if the substring is not found
  */
 char *_strstr(char *haystack, char *needle)
+/*Fonction qui cherche la première occurrence de needle dans haystack*/ 
 {
-
-	unsigned int i;
-	unsigned int j;
+	unsigned int i;   /* position de départ testée dans haystack */
+	unsigned int j;   /* position dans needle, pour la comparaison */
 
 	i = 0;
 	while (haystack[i] != '\0')
 	{
 		j = 0;
+		/* tant qu'on n'a pas atteint la fin de needle
+		   ET que les caractères correspondent encore... */
 		while (needle[j] != '\0' && haystack[i + j] == needle[j])
 		{
-			j++;
+			j++; // Parcourt needle et haystack en parallèle
 		}
+		/* si on est sorti de la boucle PARCE QUE needle est fini
+		   (donc tous les caractères ont matché), on a trouvé ! */
 		if (needle[j] == '\0')
 		{
-			return (&haystack[i]);
+			return (&haystack[i]); // Si fin de needle atteinte, retourne pointeur
 		}
-		i++;
+		i++; // Passe au caractère suivant
 	}
 	return (NULL);
 }
