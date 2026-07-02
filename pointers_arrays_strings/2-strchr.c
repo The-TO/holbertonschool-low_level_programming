f0b98e39f6a3c089d789283453093cdbe380fe29
The-TO <theovinc@gmail.com>
2026-07-02 12:11:20 +0200

    feat: implement _strchr function and update main to demonstrate usage

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * _strchr - Trouve la première occurrence d'un caractère dans une chaîne
+ * @s: Pointeur sur la chaîne à parcourir
+ * @c: Le caractère à localiser
+ *
+ * Return: Pointeur vers le caractère trouvé, ou NULL si absent
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+		i++;
+	}
+	return (NULL);
+}
