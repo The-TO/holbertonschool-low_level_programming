a2275146a1e4927497eac060a7a734672da55446
The-TO <theovinc@gmail.com>
2026-07-02 16:52:37 +0200

    feat: implement _strstr function to locate a substring and update main to demonstrate usage

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ * @haystack: string to search in
+ * @needle: substring to find
+ *
+ * Return: pointer to the beginning of the located substring,
+ * or NULL if the substring is not found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+
+	unsigned int i;
+	unsigned int j;
+
+	i = 0;
+	while (haystack[i] != '\0')
+	{
+		j = 0;
+		while (needle[j] != '\0' && haystack[i + j] == needle[j])
+		{
+			j++;
+		}
+		if (needle[j] == '\0')
+		{
+			return (&haystack[i]);
+		}
+		i++;
+	}
+	return (NULL);
+}
