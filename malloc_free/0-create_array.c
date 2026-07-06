afce8817282e3eb0cdfcedaff9d35fbfc06d9b9e
The-TO <theovinc@gmail.com>
2026-07-06 16:57:40 +0200

    fix: improve create_array function by adding null check for malloc and refactor code structure; add main function for testing

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -11,13 +11,18 @@
 char *create_array(unsigned int size, char c)
 {
 	int i;
-	char *array = malloc(size * sizeof(char));
+	char *array;
 
 	if (size == 0)
 	{
 		return (NULL);
 	}
-	for (i = 0; i < size ; i++)
+	array = malloc(size * sizeof(char));
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0 ; i < size ; i++)
 	{
 		array[i] = c;
 	}
