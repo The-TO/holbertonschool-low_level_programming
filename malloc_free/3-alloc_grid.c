5e9b322f483fa1103094e47f3432de18b1242eae
The-TO <theovinc@gmail.com>
2026-07-06 22:42:04 +0200

    feat: implement alloc_grid function to allocate a 2D grid of integers and add main function for testing

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * alloc_grid - Alloue une grille 2D d'entiers initialisés à 0
+ * @width: Nombre de colonnes
+ * @height: Nombre de lignes
+ *
+ * Return: Pointeur vers la grille allouée, NULL en cas d'erreur
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int i;
+	int j;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	grid = malloc(height * sizeof(int *));
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < height ; i++)
+	{
+		grid[i] = malloc(width * sizeof(int));
+		if (grid[i] == NULL)
+		{
+			for (j = 0 ; j < i ; j++)
+			free(grid[j]);
+			free(grid);
+			return (NULL);
+		}
+		for (j = 0 ; j < width ; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
+	return (grid);
+}
