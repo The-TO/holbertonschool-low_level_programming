4c2bd78df086bebfafc943032e5a84857e5f4885
The-TO <theovinc@gmail.com>
2026-07-06 22:57:34 +0200

    feat: implement free_grid function to free a 2D grid and add main function for testing

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * free_grid - Libère une grille 2D précédemment créée par alloc_grid
+ * @grid: Pointeur vers la grille 2D à libérer
+ * @height: Nombre de lignes de la grille
+ *
+ * Return: void
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0 ; i < height ; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
