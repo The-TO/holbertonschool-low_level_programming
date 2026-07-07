964dd77dde64a8d64bd98ad6d14888f3938e62a1
The-TO <theovinc@gmail.com>
2026-07-07 10:01:46 +0200

    feat: implement malloc_checked function for dynamic memory allocation with error handling

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * malloc_checked - Alloue de la mémoire et gère les erreurs
+ * @b: Nombre de bytes à allouer
+ *
+ * Description: Alloue dynamiquement b bytes de mémoire à l'aide de malloc.
+ * Si l'allocation échoue, la fonction cause la terminaison du processus
+ * avec le code de sortie 98.
+ *
+ * Return: Pointeur vers la mémoire allouée
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
+}
