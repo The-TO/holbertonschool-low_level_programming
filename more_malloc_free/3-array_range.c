9427c48f758de82299cd7131e551d2b98358e720
The-TO <theovinc@gmail.com>
2026-07-07 19:07:22 +0200

    fix: correct array initialization in array_range function

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -15,7 +15,24 @@
  */
 
 int *array_range(int min, int max)
+{
+	int *arr;
+	int size;
+	int i;
 
-int *arr;
-int size;
-int i;
+	if (min > max)
+	{
+		return (NULL);
+	}
+	size = (max - min) + 1;
+	arr = malloc(size * sizeof(int));
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0 ; i < size ; i++)
+	{
+		arr[i] = min + 1;
+	}
+	return (arr);
+}
