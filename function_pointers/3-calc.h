087a86c633323206f02519b785c69d0293d75587
The-TO <theovinc@gmail.com>
2026-07-15 16:18:59 +0200

    feat: implement main function for calculator and error handling

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -1,26 +1,42 @@
-#ifndef CALC_H
-#define CALC_H
-
-#include <stdio.h>
-#include <stdlib.h>
+#include "3-calc.h"
 
 /**
- * struct op - Struct op
- *
- * @op: The operator
- * @f: The function associated
+ * main - Entry point, performs calculations
+ * @argc: Argument count
+ * @argv: Argument vector
+ * Return: Always 0 on success
  */
-typedef struct op
+
+int main(int argc, char *argv[])
 {
-	char *op;
-	int (*f)(int a, int b);
-} op_t;
+	int num1, num2, result;	  // déclare les variables num1, num2 et result
+	int (*op_func)(int, int); // déclare un pointeur vers une fonction prenant 2 int
 
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
-int (*get_op_func(char *s))(int, int);
+	if (argc != 4) // vérifie que argc est bien égal à 4
+	{
+		printf("Error\n"); // affiche "Error" suivi d'un retour à la ligne
+		exit(98);		   // quitte le programme avec le code 98
+	}
 
-#endif
+	num1 = atoi(argv[1]);			// convertit argv[1] en entier et le stocke dans num1
+	num2 = atoi(argv[3]);			// convertit argv[3] en entier et le stocke dans num2
+	op_func = get_op_func(argv[2]); // récupère la fonction correspondant à l'opérateur donné
+
+	if (op_func == NULL) // vérifie si l'opérateur n'est pas valide
+	{
+		printf("Error\n"); // affiche "Error"
+		exit(99);		   // quitte avec le code 99
+	}
+
+	// vérifie si on tente une division ou un modulo par 0
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
+	{
+		printf("Error\n"); // affiche "Error"
+		exit(100);		   // quitte avec le code 100
+	}
+
+	result = op_func(num1, num2); // appelle la fonction d’opération avec num1 et num2
+	printf("%d\n", result);		  // affiche le résultat suivi d’un retour à la ligne
+
+	return (0); // indique que le programme s’est exécuté correctement
+}
