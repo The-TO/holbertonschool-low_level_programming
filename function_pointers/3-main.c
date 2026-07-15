10d80391995c8ed388720b3c145b6a42215f1737
The-TO <theovinc@gmail.com>
2026-07-15 16:16:34 +0200

    fix: remove redundant return statement in main function

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -15,22 +15,21 @@ int main(int argc, char *argv[])
 	{
 		printf("Error\n");
 		exit(98);
-		num1 = atoi(argv[1]);
-		num2 = atoi(argv[3]);
-		op_func = get_op_func(argv[2]);
-		if (op_func == NULL)
-		{
-			printf("Error\n");
-			exit(99);
-		}
-		if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
-		{
-			printf("Error\n");
-			exit(100);
-		}
-		result = op_func(num1, num2);
-		printf("%d\n", result);
-		
 	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	op_func = get_op_func(argv[2]);
+	if (op_func == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	result = op_func(num1, num2);
+	printf("%d\n", result);
 	return (0);
 }
