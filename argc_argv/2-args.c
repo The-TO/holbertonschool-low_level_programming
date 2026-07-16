015f49bb83e92d14289f302704eff188e8336cc0
The-TO <theovinc@gmail.com>
2026-07-16 18:15:45 +0200

    fix: correct syntax errors in argument printing loop in 2-args.c

---
+++ argc_argv/2-args.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints all arguments passed into the program
+ * @argc: the number of arguments passed to the program
+ * @argv: an array of strings containing the arguments
+ *
+ * Description: This program prints each command-line argument
+ * on a separate line, including the program name.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0 ; i < argc)
+	{
+		printf("%s\n", argv[i])
+		i++
+	}
+	return (0);
+}
