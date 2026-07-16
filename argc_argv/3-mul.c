4cffc4131f451927ae1361b5ec9b8bff442bc4a5
The-TO <theovinc@gmail.com>
2026-07-16 18:26:18 +0200

    fix: standardize indentation in 3-mul.c

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -15,16 +15,16 @@
  */
 int main(int argc, char *argv[])
 {
-    int num1;
-    int num2;
+	int num1;
+	int num2;
 
-    if (argc != 3)
-    {
-        prtinf("Error\n");
-        return (1);
-    }
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[2]);
-    printf("%d", num1 * num2);
-    return(0);
+	if (argc != 3)
+	{
+		prtinf("Error\n");
+		return (1);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	printf("%d", num1 * num2);
+	return (0);
 }
