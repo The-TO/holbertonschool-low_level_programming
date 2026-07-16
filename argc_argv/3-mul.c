46eb48343808b9ef6c4622ce43ab81783b765dfa
The-TO <theovinc@gmail.com>
2026-07-16 18:25:52 +0200

    fix: correct typo in error message function call in 3-mul.c

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point of the program
+ * @argc: The number of arguments passed to the program
+ * @argv: An array of strings containing the arguments
+ *
+ * Description: Multiplies two integer numbers passed as command line arguments
+ * and prints the result. If the
+ * number of arguments is incorrect, prints "Error"
+ * and returns 1.
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char *argv[])
+{
+    int num1;
+    int num2;
+
+    if (argc != 3)
+    {
+        prtinf("Error\n");
+        return (1);
+    }
+    num1 = atoi(argv[1]);
+    num2 = atoi(argv[2]);
+    printf("%d", num1 * num2);
+    return(0);
+}
