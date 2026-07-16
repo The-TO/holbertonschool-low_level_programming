0af4cce831c7392688566c7841cef8eca8c391d7
The-TO <theovinc@gmail.com>
2026-07-16 18:00:37 +0200

    feat: add initial implementation of program name printing

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the program name
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	printf('%d\n', argv[0]);
+	return (0);
+}
+
