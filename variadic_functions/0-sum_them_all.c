7b66896dec12b2cce2d8bae0538b6d273c65a39f
The-TO <theovinc@gmail.com>
2026-07-16 10:52:28 +0200

    feat: implement sum_them_all function and update header file

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+/**
+ * sum_them_all - Sums all its parameters
+ * @n: Number of parameters to sum
+ *
+ * Return: Sum of all parameters, or 0 if n is 0
+ */
+
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	int sum;
+	unsigned int i;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+	sum = 0;
+	va_start(args, n);
+	for (i = 0 ; i < n ; i++)
+	{
+		sum += va_arg(args, int);
+	}
+	va_end(args);
+	return (sum);
+}
