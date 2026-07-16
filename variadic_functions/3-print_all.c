50d80ff8995ae7c151239f9cf228c478c16843f8
The-TO <theovinc@gmail.com>
2026-07-16 16:11:37 +0200

    fix: enhance comments for clarity in print_all function

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -21,17 +21,18 @@
 
 void print_all(const char *const format, ...)
 {
-	va_list args;
-	unsigned int i;
-	char c;
-	char *s;
-	char *sep = ", ";
+	va_list args;						// Création d'une liste pour stocker les arguments variables
+	unsigned int i;						// Compteur pour parcourir le tableau 'format'
+	char c;								// Pour stocker un caractère du format
+	char *s;							// Pointeur vers une chaîne de caractères
+	char *sep = ", ";					/* Séparateur affiché après chaque élément */
 
 	va_start(args, format);
 	i = 0;
-	while (format && format[i])
+	while (format && format[i]).   		//format = quand il ne vaut rien (NULL) et format[i] à une valeur
 	{
 		c = format[i];
+		/* Si c'est le dernier caractère valide du format, pas de séparateur après */
 		if ((c == 'c' || c == 'i' || c == 'f' || c == 's') && format[i + 1] == '\0')
 		{
 			sep = "";
