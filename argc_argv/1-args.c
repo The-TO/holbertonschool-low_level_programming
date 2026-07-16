b622c85fc9b3b2a502c95f812facf7666f0e677a
The-TO <theovinc@gmail.com>
2026-07-16 18:09:46 +0200

    feat: implement program to count and print command-line arguments

---
+++ argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the number of arguments passed into the program
+ * @argc: the number of arguments passed to the program
+ * @argv: an array of strings containing the arguments
+ *
+ * Description: This program counts and displays the total number of
+ * command-line arguments passed to it, including the program name itself.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%d\n", argc - 1);
+	return (0);
