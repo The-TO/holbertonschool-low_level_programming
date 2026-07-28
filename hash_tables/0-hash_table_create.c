ed17d489db653516f25af4dd45e8e232ab2a5b28
The-TO <theovinc@gmail.com>
2026-07-28 16:04:08 +0200

    feat: add hash table data structures and creation function

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,9 @@
+#include "hash_tables.h"
+#include "stdlib.h"
+
+/**
+ * hash_table_create - Function that creates a hashtable
+ * @size: Size of the table
+ * Return: theTABLE (Created the table)
+ */
+hash_table_t *hash_table_create(unsigned long int size)
