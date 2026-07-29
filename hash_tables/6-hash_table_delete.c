5273fe8f2d6d08288525c92a9a661447dca99d7a
The-TO <theovinc@gmail.com>
2026-07-29 19:19:06 +0200

    feat: implement hash_table_delete function to free hash table memory

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+
+/**
+ * hash_table_delete - Deletes a hash table
+ * @ht: Hash table
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *node;
+	hash_node_t *tmp;
+
+	if (!ht)
+	{
+		return;
+	}
+
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node)
+		{
+			tmp = node->next;
+			free(node->key);
+			free(node->value);
+			free(node);
+			node = tmp;
+		}
+	}
+
+	free(ht->array);
+	free(ht);
+}
